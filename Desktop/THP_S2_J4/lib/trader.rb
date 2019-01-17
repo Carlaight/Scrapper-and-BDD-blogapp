7cd50a968f3bf4f456bb35962d83e4e832b6a3a9
carlaight <charlesmix23@gmail.com>
2019-01-17 22:26:28 +0100

    update

--- Desktop/THP_S2_J4/lib/trader.rb
+++ Desktop/THP_S2_J4/lib/trader.rb
@@ -2,30 +2,32 @@ require 'rubygems'
 require 'open-uri'
 require 'nokogiri'
 require 'pry'
-
+	
 def crypto_scrapper
-
+	
 	crypto = []
 	page = Nokogiri::HTML(open("https://coinmarketcap.com/all/views/all/"))
 	page.xpath('//*/td[2]/a').each do |node|
 	crypto << node.text
 	end
-
+	
 	value = []
 	page = Nokogiri::HTML(open("https://coinmarketcap.com/all/views/all/"))
 	page.xpath('//*/td[5]/a').each do |node|
 	value << node.text
 	end
-	
+		
 	nbcrypto = crypto.length  # Pour s'assurer que les 2 arrays son t égales
 	#nbvalue = value.length
-	
+		
 	cryptovalue = []
 	nbcrypto.times do |i|
-    cryptovalue << { "#{crypto[i]}" => value[i] }
+		cryptovalue << { "#{crypto[i]}" => value[i] }
 	end
 	# cryptovalue = Hash[crypto.zip value] #création du hash cryptovalue qui associe value à crypto
-	puts "#{cryptovalue}"
+	# puts "#{cryptovalue}"
+	puts "#{cryptovalue[1][1]}"
+	return cryptovalue
 end
-
+	
 crypto_scrapper
