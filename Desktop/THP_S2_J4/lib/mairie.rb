da1695a2b4bdfce6da4b8573dec7e6b4753d9b2b
carlaight <charlesmix23@gmail.com>
2019-01-17 23:05:18 +0100

    update

--- Desktop/THP_S2_J4/lib/mairie.rb
+++ Desktop/THP_S2_J4/lib/mairie.rb
@@ -3,41 +3,47 @@ require 'open-uri'
 require 'nokogiri'
 require 'pry'
 
-def get_townhall_email(get_townhall_urls)
+begin
+  do_something() 
+rescue StandardError => e #Pas d'erreur si pas d'email
+	puts "Toutes les mairies n'ont pas renseigné leur email" 
+end
+page = Nokogiri::HTML(open("http://annuaire-des-mairies.com/val-d-oise.html"))
 
-	@townhall_emails = []
-	page = Nokogiri::HTML(open("http://annuaire-des-mairies.com/95/ableiges.html"))
-	page.xpath('/html/body/div/main/section[2]/div/table/tbody/tr[4]/td[2]').each do |node|
-	townhall_emails << node.text
-  end
-  puts "#{townhall_emails}"
-  return townhall_emails
+# Method de recherche d'email, avec l'adresse html de chaque ville
+def get_townhall_email(townhall_url)
+  email = townhall_url.xpath("/html/body/div/main/section[2]/div/table/tbody/tr[4]/td[2]")
+  return email.text
 end
 
-
-
-def get_townhall_urls
-
-	@townhall_urls = []
-	page = Nokogiri::HTML(open("http://annuaire-des-mairies.com/val-d-oise.html"))
-	page.css("a[@class=lientxt]/@href").each do |node|
-	townhall_urls << node.text
+# Method principale
+def get_townhall_urls(page)
+  # Initialisation des arrays
+  array_fin = []
+  villes = []
+  emails = []
+  # Boucle array villes
+  page.css("td/p/a[@class=lientxt]").each do |ville|
+    ville = ville.text
+    villes << ville
   end
-  puts "#{townhall_urls}"
-  return townhall_urls
+  # Boucle array emails. Recupération des url
+  page.css("td/p/a[@class=lientxt]/@href").each do |url|
+    url = url.text
+    # Retrait du point devan les liens
+    url.slice!(0)
+    # Ajout du début du lien
+    lien = "http://annuaire-des-mairies.com" + url
+    # Appel de la fonction de recherche d'email avec le nouveau lien
+    email = get_townhall_email(Nokogiri::HTML(open(lien)))
+    emails << email
+  end
+  # Création hash joignant les viles et les emails
+  hash = Hash[villes.zip(emails)]
+  # Création d'un array de hash
+  hash.each {|i| array_fin << {i[0] => i[1]}}
+	# puts array_fin
+	return array_fin
 end
 
-
-
-  nbtownhall_emails = townhall_emails.length 
-  
-	townhall_emails_all = []
-	nbtownhall_emails.times do |i|
-  townhall_emails_all << { "#{townhall_emails[i]}" => "#{townhall_urls[i]}" }
-	end
-
-  puts "#{townhall_emails_all}"
-
-get_townhall_email(townhall_urls)
-
-get_townhall_urls
+get_townhall_urls(page)
